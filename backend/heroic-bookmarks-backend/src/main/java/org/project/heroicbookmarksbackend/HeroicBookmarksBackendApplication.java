4def27e8ab9f2c6ab422f0f9095c5487afb9c386
Vishnu Kumar Verma <blueunova2003@gmail.com>
2025-06-21 14:15:38 +0530

    Remove the .env dependencies from the application

--- backend/heroic-bookmarks-backend/src/main/java/org/project/heroicbookmarksbackend/HeroicBookmarksBackendApplication.java
+++ backend/heroic-bookmarks-backend/src/main/java/org/project/heroicbookmarksbackend/HeroicBookmarksBackendApplication.java
@@ -13,23 +13,7 @@ import java.util.Map;
 
 @SpringBootApplication
 public class HeroicBookmarksBackendApplication {
-
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.configure().load();
-        Map<String, Object> envMap = new HashMap<>();
-        dotenv.entries().forEach(entry -> envMap.put(entry.getKey(), entry.getValue()));
-
-        // Create SpringApplication instance
-        SpringApplication app = new SpringApplication(HeroicBookmarksBackendApplication.class);
-
-        // Inject the dotenv properties into Spring Environment before startup
-        app.addInitializers(applicationContext -> {
-            ConfigurableEnvironment env = applicationContext.getEnvironment();
-            env.getPropertySources().addFirst(new MapPropertySource("dotenvProperties", envMap));
-        });
-
-        // Run the application
-        app.run(args);
+        SpringApplication.run(HeroicBookmarksBackendApplication.class, args);
     }
 }
