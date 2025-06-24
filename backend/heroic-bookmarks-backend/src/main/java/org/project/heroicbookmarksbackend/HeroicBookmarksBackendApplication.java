be3e298fd23f994b81f3b8f9d36110c59cf4f87c
Vishnu Kumar Verma <blueunova2003@gmail.com>
2025-06-25 00:51:25 +0530

    adjusting the database

--- backend/heroic-bookmarks-backend/src/main/java/org/project/heroicbookmarksbackend/HeroicBookmarksBackendApplication.java
+++ backend/heroic-bookmarks-backend/src/main/java/org/project/heroicbookmarksbackend/HeroicBookmarksBackendApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HeroicBookmarksBackendApplication {
     public static void main(String[] args) {
-         System.out.println("DB_URL: " + System.getenv("DB_URL"));
-    System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-    System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
-    System.out.println("DATABASE_PLATFORM: " + System.getenv("DATABASE_PLATFORM"));
         SpringApplication.run(HeroicBookmarksBackendApplication.class, args);
     }
 }
