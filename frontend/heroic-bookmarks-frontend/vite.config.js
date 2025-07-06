519fe151f8d8d190b737b92c7442283b4d52bd39
Vishnu Kumar Verma <blueunova2003@gmail.com>
2025-07-06 23:49:17 +0530

    adjusting the vite config file to set the base url for the site

--- frontend/heroic-bookmarks-frontend/vite.config.js
+++ frontend/heroic-bookmarks-frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { qrcode } from 'vite-plugin-qrcode';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { qrcode } from "vite-plugin-qrcode";
 
 export default defineConfig({
   plugins: [react(), qrcode()],
+  base: "/",
   server: {
-    host: true // exposes local IP so you can open it on phone
-  }
+    host: true, // exposes local IP so you can open it on phone
+  },
 });
